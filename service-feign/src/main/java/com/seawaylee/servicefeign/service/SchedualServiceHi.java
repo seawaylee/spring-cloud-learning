ab67559b4d977982d439afe0ef667b3caef16f4e
lixiwei <13522781970@163.com>
2018-03-21 13:43:14 +0800

    Ribbon+Feign+2Client+Server 完成;

---
+++ service-feign/src/main/java/com/seawaylee/servicefeign/service/SchedualServiceHi.java
@@ -0,0 +1,14 @@
+package com.seawaylee.servicefeign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "SERVICE-HI")
+@Service
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
